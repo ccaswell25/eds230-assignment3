d87cf4466ad2053106646af101d6c326e0ca93cf
ccaswell25 <ccaswell25@gmail.com>
2024-04-11 08:48:54 -0700

    updating format

--- profit_model.R
+++ profit_model.R
@@ -1,5 +1,5 @@
 #Almond Profit Model
 
-#you might assume a baseline profit and then adjust according to the anomaly  
-
-#there are many ways to combine the almond yield and profit functions; you can have the profit function "call"/use the almond yield function; or create a wrapper function that calls them in sequence (first the almond yield and then the profit function) 
+##Create function for profit
+####Use the almond yield function output as an input using "call"
+baseline = x

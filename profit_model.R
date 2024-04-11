726850332035cc1abfff44705d2cf9531427be51
ccaswell25 <ccaswell25@gmail.com>
2024-04-11 09:06:45 -0700

    added npv

--- profit_model.R
+++ profit_model.R
@@ -1,5 +1,12 @@
 #Almond Profit Model
 
+##Compute NPV
+function (value, time, discount = 0.12)
+   {
+   result = value/(1 + discount)^time
+   return(result)
+}
+
 ##Create function for profit
 ####Use the almond yield function output as an input using "call"
 baseline = x

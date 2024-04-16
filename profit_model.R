22d7023a87397489674159486930a1c471ec19ad
ccaswell25 <ccaswell25@gmail.com>
2024-04-16 06:59:23 -0700

    started function

--- profit_model.R
+++ profit_model.R
@@ -10,3 +10,16 @@ function (value, time, discount = 0.12)
 ##Create function for profit
 ####Use the almond yield function output as an input using "call"
 baseline = x
+
+#Call almond yield data
+yield_data <- 
+
+# Function to calculate almond profit
+calculate_almond_profit <- function(yield_data, cost_per_hectare, price_per_kg) {
+  revenue <- yield_data * price_per_kg
+  total_cost <- cost_per_hectare
+  # Calculate profit
+  profit <- revenue - total_cost
+  
+  return(profit)
+}

e49ce99741060767922af861befc5c1ee9eee11e
ccaswell25 <ccaswell25@gmail.com>
2024-04-16 12:41:34 -0700

    adding some deets on model

--- profit_model.R
+++ profit_model.R
@@ -1,25 +1,44 @@
-#Almond Profit Model
+#' Almond Profit Function
+#' Computes the almond profit from the almond yield anomoly in response to climate
 
-##Compute NPV
-function (value, time, discount = 0.12)
-   {
-   result = value/(1 + discount)^time
-   return(result)
-}
+#' @param  yield_data from almond_yield function
+#' @param  cost_per_hectare default = 100
+#' @param  price_per_kg default = 200
+#' @param  discount_rate
+#' @param  years 
+#' @return profit, npv 
+#' 
+#' @author Annie Adams and Carly Caswell
+
 
 ##Create function for profit
 ####Use the almond yield function output as an input using "call"
-baseline = x
 
-#Call almond yield data
-yield_data <- 
+# Call almond yield data
+source("almond_yield.R")
+almond_yield
 
+  
 # Function to calculate almond profit
-calculate_almond_profit <- function(yield_data, cost_per_hectare, price_per_kg) {
+calculate_almond_profit <- function(input_data, cost_per_hectare = 100, price_per_kg = 200, discount_rate, years) {
+  
+  #Compute almond yield
+  yield_data <- almond_yield(input_data)
+  
+  #Calculate Revenue
   revenue <- yield_data * price_per_kg
+  
+  #Define Cost
   total_cost <- cost_per_hectare
-  # Calculate profit
+  
+  # Calculate Profit
   profit <- revenue - total_cost
   
-  return(profit)
+  # Calculate Net Present Value
+  discount_factors <- (1 + discount_rate)^(-1 * (1:years))
+  discounted_cash_flows <- c(-total_cost, rep(profit, years - 1)) * discount_factors
+  npv <- sum(discounted_cash_flows)
+  
+  return(print(paste0("Profit ($): ", profit,  "NPV ($): ",npv)))
+  
 }

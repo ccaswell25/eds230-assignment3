04282d1a87f6a01e255b027c656a4e122f0e8a88
ccaswell25 <ccaswell25@gmail.com>
2024-04-16 17:47:28 -0700

    updates to model and sensitivity analysis

--- profit_model.R
+++ profit_model.R
@@ -1,7 +1,7 @@
 #' Almond Profit Function
 #' Computes the almond profit from the almond yield anomoly in response to climate
 
-#' @param  yield_data from almond_yield function
+#' @param  yield_data output from almond_yield function
 #' @param  cost_per_hectare default = 100
 #' @param  price_per_kg default = 200
 #' @param  discount_rate
@@ -11,19 +11,18 @@
 #' @author Annie Adams and Carly Caswell
 
 
-##Create function for profit
-####Use the almond yield function output as an input using "call"
+##Create a function for profit
 
-# Call almond yield data
+# Call almond yield function
 source("almond_yield.R")
 almond_yield
 
   
 # Function to calculate almond profit
-calculate_almond_profit <- function(input_data, cost_per_hectare = 100, price_per_kg = 200, discount_rate, years) {
+calculate_almond_profit <- function(cost_per_hectare = 100, price_per_kg = 200, discount_rate, years) {
   
   #Compute almond yield
-  yield_data <- almond_yield(input_data)
+  yield_data <- almond_yield(clim,Tmincoeff1=-0.015, Tmincoeff2=-0.0046, Pcoeff1=-0.07, Pcoeff2=0.0043, intercep=0.28)
   
   #Calculate Revenue
   revenue <- yield_data * price_per_kg
@@ -42,3 +41,4 @@ calculate_almond_profit <- function(input_data, cost_per_hectare = 100, price_pe
   return(print(paste0("Profit ($): ", profit,  "NPV ($): ",npv)))
   
 }
+

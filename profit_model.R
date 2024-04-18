e47666b8394df615ec2b144944c5db5e21dee9df
annieradams <adams.annie11@gmail.com>
2024-04-17 21:13:22 -0700

    need to figure out why profit model is returning profit where year column is

--- profit_model.R
+++ profit_model.R
@@ -18,29 +18,33 @@ source("almond_yield_function.R")
 almond_yield
 
   
-# Function to calculate almond profit
-calculate_almond_profit <- function(df, 
-                                    t_min_coef_1 = -0.015, 
-                                    t_min_coef_2 = -0.0046, 
-                                    precip_coef_1 = -0.07, 
-                                    precip_coef_2 = 0.0043, 
-                                    intercept = 0.28, cost_per_hectare = 100, price_per_kg = 200, year) {
+#Function to calculate almond profit
+calculate_almond_profit <- function(df,
+                                    Tmincoeff1 = -0.015,
+                                    Tmincoeff2 = -0.0046,
+                                    Pcoeff1 = -0.07,
+                                    Pcoeff2 = 0.0043,
+                                    intercept = 0.28, cost_per_hectare = 100, price_per_kg = 200, hectare = 3) {
+  
+
   
   #Compute almond yield
   yield_data <- almond_yield(df,Tmincoeff1, Tmincoeff2, Pcoeff1, Pcoeff2, intercept)
   
+  total_yield = yield_data * hectare
+  
+  profit = (total_yield * price_per_kg) - (total_yield * cost_per_hectare)
+  
+  # #Calculate Revenue
+  # revenue <- yield_data * price_per_kg
+  # 
+  # #Define Cost
+  # total_cost <- cost_per_hectare
+  # 
+  # # Calculate Profit
+  # profit <- revenue - total_cost
 
-  
-  #Calculate Revenue
-  revenue <- yield_data * price_per_kg
-  
-  #Define Cost
-  total_cost <- cost_per_hectare
-  
-  # Calculate Profit
-  profit <- revenue - total_cost
-
-  return(profit)
+  return(list(profit = profit, mean_profit = mean(profit)))
   
 }
 
